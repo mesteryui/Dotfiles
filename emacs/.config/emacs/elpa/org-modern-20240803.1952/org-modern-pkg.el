b8ff1d5d76aea3a784c720cc1dfb16f3a29ad0ea
Oscar <oscarodriguez56@gmail.com>
2024-08-19 16:12:01 +0200

    Modificaciones config

---
+++ emacs/.config/emacs/elpa/org-modern-20240803.1952/org-modern-pkg.el
@@ -0,0 +1,15 @@
+(define-package "org-modern" "20240803.1952" "Modern looks for Org"
+  '((emacs "27.1")
+    (compat "30"))
+  :commit "e306c7df4985f77e5c4e2146900259a23a76c974" :authors
+  '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainers
+  '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainer
+  '("Daniel Mendler" . "mail@daniel-mendler.de")
+  :keywords
+  '("outlines" "hypermedia" "text")
+  :url "https://github.com/minad/org-modern")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
