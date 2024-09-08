9802271f076ec5c986c988aaf0b5b02c25327f00
Oscar <oscarodriguez56@gmail.com>
2024-09-08 20:11:20 +0200

    Script facilitar instalacion en nueva maquina

---
+++ post_install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+sudo pacman -S hyprland pipewire-pulse pipewire-alsa kitty thunar sddm
+
+# Segunda fase
+sudo pacman -S syncthing pavucontrol ttf-jetbrains-mono ttf-space-mono-nerd ttf-ubuntu-font-family waybar nwg-bar ttf-font-awesome 
