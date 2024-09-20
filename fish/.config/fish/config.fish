77d2f2931c5a2240ccaa587210a818d81f4fe545
Oscar <oscarodriguez56@gmail.com>
2024-09-20 22:52:22 +0200

    Adaptacion a la configuracion de Fzf

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -29,4 +29,8 @@ alias "yt-watch" 'yt-dlp -o "/tmp/%(title)s.%(ext)s" --restrict-filenames --spon
 #alias "cd" "z"
 set -x EDITOR "nvim"
 set -Ux FZF_DEFAULT_OPTS "\
---color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
+--color=selected-bg:#45475a \
+--multi"
