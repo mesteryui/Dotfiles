093cf49dbd392cc03ecbf8f2d3b302db1bf48fdb
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-10-19 23:50:25 +0200

    Añadiendo cambios a otros programas

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@
 local wezterm = require 'wezterm'
 local act = wezterm.action
 return {
---enable_wayland = true,
+enable_wayland = true,
 front_end = "OpenGL",
 automatically_reload_config = true,
     window_background_opacity = 0.90,
@@ -28,15 +28,22 @@ automatically_reload_config = true,
     {key = "v", mods = "ALT",action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" },},
  {key = "t", mods = "ALT",action = wezterm.action.SpawnTab "CurrentPaneDomain",},
 	{key="q", mods="ALT", action=act.CloseCurrentTab{confirm = true}},
-        {key="1", mods="CTRL|SUPER", action=act{ActivateTab=0}},
-        {key="2", mods="CTRL|SUPER", action=act{ActivateTab=1}},
-        {key="3", mods="CTRL|SUPER", action=act{ActivateTab=2}},
-        {key="4", mods="CTRL|SUPER", action=act{ActivateTab=3}},
-        {key="5", mods="CTRL|SUPER", action=act{ActivateTab=4}},
-        {key="6", mods="CTRL|SUPER", action=act{ActivateTab=5}},
-        {key="7", mods="CTRL|SUPER", action=act{ActivateTab=6}},
-        {key="8", mods="CTRL|SUPER", action=act{ActivateTab=7}},
-        {key="9", mods="CTRL|SUPER", action=act{ActivateTab=8}},
-        {key="0", mods="CTRL|SUPER", action=act{ActivateTab=9}},
-}
+        {key="1", mods="CTRL", action=act{ActivateTab=0}},
+        {key="2", mods="CTRL", action=act{ActivateTab=1}},
+        {key="3", mods="CTRL", action=act{ActivateTab=2}},
+        {key="4", mods="CTRL", action=act{ActivateTab=3}},
+        {key="5", mods="CTRL", action=act{ActivateTab=4}},
+        {key="6", mods="CTRL", action=act{ActivateTab=5}},
+        {key="7", mods="CTRL", action=act{ActivateTab=6}},
+        {key="8", mods="CTRL", action=act{ActivateTab=7}},
+        {key="9", mods="CTRL", action=act{ActivateTab=8}},
+        {key="0", mods="CTRL", action=act{ActivateTab=9}},
+},
+         ssh_domains = {
+        	    {
+            	    name = "server",
+            	    remote_address = "192.168.0.84",
+           	     username = "oscar"
+       		   }
+    }
 }
