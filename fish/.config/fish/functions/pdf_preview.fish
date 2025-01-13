62b2c31336a50ac55f1e5b3b26d613b0accd82ca
Oscar <oscarodriguez56@gmail.com>
2025-01-13 12:12:25 +0100

    Añadir funcion pdf_preview

---
+++ fish/.config/fish/functions/pdf_preview.fish
@@ -0,0 +1,10 @@
+function pdf_preview --description="previsualizar pdfs en terminal"
+	set filename "$argv[1]"
+	if test $(file -Lb --mime-type "$filename") = "application/pdf"
+		set file "$( basename $(echo "Descargas/0.1_Presentación.pdf" | tr ' ' '_'))"
+		pdftoppm -f 1 -l 1 "$filename" >> "/tmp/$file.png"
+	end
+	 kitty +kitten icat --silent --stdin no --transfer-mode file "/tmp/$file.png" < /dev/null > /dev/tty
+	 rm -r "/tmp/$file.png"
+
+ end
