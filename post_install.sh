eca382b8722135e133dae40e2cebe8f13bc6316d
Oscar <oscarodriguez56@gmail.com>
2025-01-13 10:56:57 +0100

    Cambiando la post instalacion

--- post_install.sh
+++ post_install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo pacman -S hyprland pipewire-pulse pipewire-alsa kitty thunar sddm
+sudo pacman -S pipewire-pulse pipewire-alsa kitty thunar sddm yazi
 
 # Segunda fase
-sudo pacman -S syncthing pavucontrol ttf-jetbrains-mono ttf-space-mono-nerd ttf-ubuntu-font-family waybar nwg-bar ttf-font-awesome 
+sudo pacman -S syncthing pavucontrol ttf-jetbrains-mono ttf-ubuntu-font-family waybar nwg-bar ttf-font-awesome 
