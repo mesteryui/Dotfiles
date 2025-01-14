9bd078ee54d65a2335d5f80f9b6182992aaae46a
Oscar <oscarodriguez56@gmail.com>
2025-01-14 08:31:17 +0100

    Añadiendo cosas postinstalacion

--- post_install.sh
+++ post_install.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-sudo pacman -S pipewire-pulse pipewire-alsa kitty thunar sddm yazi
-
+sudo pacman -S pipewire-pulse pipewire-alsa kitty thunar sddm yazi television
 # Segunda fase
 sudo pacman -S syncthing pavucontrol ttf-jetbrains-mono ttf-ubuntu-font-family waybar nwg-bar ttf-font-awesome 
