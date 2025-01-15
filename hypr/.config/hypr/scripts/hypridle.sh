b582f516dd2402f150499227fc38a760ce35f093
Oscar <oscarodriguez56@gmail.com>
2025-01-15 17:56:34 +0100

    Modificando script hypridle

--- hypr/.config/hypr/scripts/hypridle.sh
+++ hypr/.config/hypr/scripts/hypridle.sh
@@ -11,9 +11,9 @@ SERVICE="hypridle"
 if [[ "$1" == "status" ]]; then
     sleep 1
     if pgrep -x "$SERVICE" >/dev/null ;then
-        echo '{"text": "RUNNING", "class": "active", "tooltip": "Screen locking active\nLeft: Deactivate\nRight: Lock Screen"}'
+    	echo '{"text": "  On", "tooltip": "Hypridle esta activado"}'       
     else
-        echo '{"text": "NOT RUNNING", "class": "notactive", "tooltip": "Screen locking deactivated\nLeft: Activate\nRight: Lock Screen"}'
+        echo '{"text": "  Off", "tooltip": "Hypridle esta desactivado"}'
     fi
 fi
 if [[ "$1" == "toggle" ]]; then
