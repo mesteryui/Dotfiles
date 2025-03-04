6abbfc1733a6ca63268034d03ad31d2c08dd5acf
Oscar <oscarodriguez56@gmail.com>
2025-03-04 17:56:19 +0100

    Añadiendo colores y ajustes junto con selector de fondos

---
+++ rofi/.config/rofi/wallSelect.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#  ██╗    ██╗ █████╗ ██╗     ██╗     ██████╗  █████╗ ██████╗ ███████╗██████╗
+#  ██║    ██║██╔══██╗██║     ██║     ██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗
+#  ██║ █╗ ██║███████║██║     ██║     ██████╔╝███████║██████╔╝█████╗  ██████╔╝
+#  ██║███╗██║██╔══██║██║     ██║     ██╔═══╝ ██╔══██║██╔═══╝ ██╔══╝  ██╔══██╗
+#  ╚███╔███╔╝██║  ██║███████╗███████╗██║     ██║  ██║██║     ███████╗██║  ██║
+#   ╚══╝╚══╝ ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝     ╚═╝  ╚═╝╚═╝     ╚══════╝╚═╝  ╚═╝
+#
+#  ██╗      █████╗ ██╗   ██╗███╗   ██╗ ██████╗██╗  ██╗███████╗██████╗
+#  ██║     ██╔══██╗██║   ██║████╗  ██║██╔════╝██║  ██║██╔════╝██╔══██╗
+#  ██║     ███████║██║   ██║██╔██╗ ██║██║     ███████║█████╗  ██████╔╝
+#  ██║     ██╔══██║██║   ██║██║╚██╗██║██║     ██╔══██║██╔══╝  ██╔══██╗
+#  ███████╗██║  ██║╚██████╔╝██║ ╚████║╚██████╗██║  ██║███████╗██║  ██║
+#  ╚══════╝╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
+#	originally written by: gh0stzk - https://github.com/gh0stzk/dotfiles
+#	rewritten for hyprland by :	 develcooking - https://github.com/develcooking/hyprland-dotfiles
+#	Info    - This script runs the rofi launcher, to select
+#             the wallpapers included in the theme you are in.
+
+
+
+# Set some variables
+wall_dir="${HOME}/Imágenes/Wallpapers"
+cacheDir="${HOME}/.cache/jp/${theme}"
+rofi_command="rofi -x11 -dmenu -theme ${HOME}/.config/rofi/wallConfig.rasi"
+
+# Create cache dir if not exists
+if [ ! -d "${cacheDir}" ] ; then
+        mkdir -p "${cacheDir}"
+    fi
+
+
+
+# Convert images in directory and save to cache dir
+for imagen in "$wall_dir"/*.{jpg,jpeg,png,webp}; do
+	if [ -f "$imagen" ]; then
+		nombre_archivo=$(basename "$imagen")
+			if [ ! -f "${cacheDir}/${nombre_archivo}" ] ; then
+				convert -strip "$imagen" -thumbnail 500x500^ -gravity center -extent 500x500 "${cacheDir}/${nombre_archivo}"
+			fi
+    fi
+done
+
+# Select a picture with rofi
+wall_selection=$(find "${wall_dir}"  -maxdepth 1  -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.webp" \) -exec basename {} \; | sort | while read -r A ; do  echo -en "$A\x00icon\x1f""${cacheDir}"/"$A\n" ; done | $rofi_command)
+
+# Set the wallpaper
+[[ -n "$wall_selection" ]] || exit 1
+matugen image ${wall_dir}/${wall_selection}
+
+exit 0
