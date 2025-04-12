d3c211883f0039c1add616b0656c24b129eeddd8
Oscar <oscarodriguez56@gmail.com>
2025-04-12 11:31:56 +0200

    Añadiendo los nuevos ajustes de forma más general

--- emacs/.config/emacs/scripts/org-utilities.el
+++ emacs/.config/emacs/scripts/org-utilities.el
@@ -1,36 +1,27 @@
 (require 'org-tempo)
-      (use-package org-appear
-      :hook
-      (org-mode . org-appear-mode))
+             (use-package org-appear
+             :hook
+             (org-mode . org-appear-mode))
 
-   (use-package org-superstar
-   :demand t  
-   :ensure t
-   :config
-  (add-hook 'org-mode-hook (lambda () (org-superstar-mode 1)))
-  (setq org-ellipsis "▼")
-  (setq org-superstar-headline-bullets-list '("◉" "●" "○" "◆" "●" "○" "◆"))
-  (setq org-superstar-item-bullet-alist '((?+ . ?➤) (?- . ?✦))))
-  ;; Modernise Org mode interface
-;  (use-package org-modern
-;    :hook
-;    (org-mode . global-org-modern-mode)
-;    :custom
-;    (org-modern-keyword nil)
-;    (org-modern-checkbox nil)
-;    (org-modern-table nil))
-      (use-package org-fragtog
-      :after org
-      :custom
-      (org-startup-with-latex-preview t)
+;; ;; Modern Org mode interface
+    (use-package org-modern
       :hook
-      (org-mode . org-fragtog-mode)
+      (org-mode . org-modern-mode)
       :custom
-      (org-format-latex-options
-       (plist-put org-format-latex-options :scale 2)
-       (plist-put org-format-latex-options :foreground 'auto)
-       (plist-put org-format-latex-options :background 'auto)))
-      (use-package ox-epub
-      :demand t)
-    (use-package ox-reveal)
-(provide 'org-utilities)
+      (org-modern-keyword t)
+      (org-modern-checkbox t)
+      (org-modern-table nil)
+      (org-modern-star nil)
+      (org-modern-todo nil))
+
+(use-package org-superstar
+            :demand t  
+            :ensure t
+	    :config
+	    (setq org-superstar-headline-bullets-list '("◉" "●" "○" "✿" "󰓎" "" ""))
+            (setq org-superstar-item-bullet-alist '((?+ . ?➤) (?- . ?✦)))
+            (add-hook 'org-mode-hook (lambda () (org-superstar-mode 1))))
+             (use-package ox-epub
+             :demand t)
+           (use-package ox-reveal)
+       (provide 'org-utilities)
