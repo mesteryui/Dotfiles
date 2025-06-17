b857a694547945e7cf14d66be4d548621dd10260
Oscar <oscarodriguez56@gmail.com>
2025-06-17 23:26:56 +0200

    Añadiendo cosas

--- rofi/.config/rofi/scripts/rofi-launcher-layout-pick.sh
+++ rofi/.config/rofi/scripts/rofi-launcher-layout-pick.sh
@@ -11,10 +11,10 @@ DEFAULT="default_conf.rasi"
 THEMES=$(ls "$THEME_DIR"/*.rasi | xargs -n1 basename)
 
 # Mostrar selector con Rofi
-SELECTED=$(echo "$THEMES" | rofi -dmenu -p "Selecciona un tema de Rofi")
+SELECTED=$(echo "$THEMES" | rofi -dmenu -p "Layout Actual: $(cat $CURRENT)" -theme "~/.config/rofi/hyprsphere-selector-layout.rasi")
 
 # Si se seleccionó uno, guardarlo
 if [[ -n "$SELECTED" ]]; then
     echo "$SELECTED" > "$CURRENT"
-    notify-send -t 3000 "HyprSphere" "Cambiando tema de rofi" --icon="$HOME/.dotfiles/logo-hyprsphere-min.png"
+    notify-send -t 3000 "HyprSphere" "Cambiando layout" --icon="$HOME/.dotfiles/logo-hyprsphere-min.png"
 fi
