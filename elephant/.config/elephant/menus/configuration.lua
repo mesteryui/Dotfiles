625b463f9ed3c3603251ffb50e916419b5874252
Oscar <oscarodriguez56@gmail.com>
2025-11-26 18:48:31 +0100

    Añadiendo configs diversa

---
+++ elephant/.config/elephant/menus/configuration.lua
@@ -0,0 +1,34 @@
+Name = "configuration"
+NamePretty = "Configuración"
+Icon = ""
+Parent = "system"
+SearchName = true
+
+function GetEntries()
+    return {
+	{
+	    Text = "Cambiar atajos teclado",
+	    Icon = "",
+	    Actions = { 
+		["cambiar_atajos_teclado"] = "ghostty -e nano $XDG_CONFIG_HOME/hypr/configs/keybinds.conf" },
+	},
+	{
+	    Text = "Permisos del compositor",
+	    Icon = "",
+	    Actions = { 
+		["permisos_del_compositor"] = "ghostty -e nano $XDG_CONFIG_HOME/hypr/configs/permissions.conf" },
+	},
+	{
+	    Text = "Configuracion del monitor",
+	    Icon = "󰍹",
+	    Actions = { 
+		["configuracion_del_monitor"] = "ghostty -e nano $XDG_CONFIG_HOME/hypr/configs/monitors.conf" },
+	},
+	{
+	    Text = "Cambiar DNS",
+	    Icon = "󰇖",
+	    Actions = { 
+		["cambiar_dns"] = "ghostty -e sudo nano /etc/systemd/resolved.conf" },
+	},
+    }
+end
---
+++ elephant/.config/elephant/menus/wallpapers.lua
@@ -0,0 +1,33 @@
+Name = "wallpapers"
+NamePretty = "Wallpapers"
+HideFromProviderlist = true
+Cache = false
+GlobalSearch = false
+
+function ApplyWallpaper(wallpaper)
+    os.execute("swww img "..wallpaper .. " --transition-type center")
+    os.execute("matugen image "..wallpaper)
+end
+
+function GetEntries()
+    local entries = {}
+    local wallpapers_dir = os.getenv("HOME") .. "/Imágenes/Wallpapers"
+    local wallpaper_see = io.popen("find '"
+			.. wallpapers_dir .. "' -maxdepth 1 -type f \\( -iname '*.png' -o -iname '*.jpg' -o -iname '*.jpeg' -o -iname '*.webp' -o -iname '*.gif' \\) | sort")
+    if wallpaper_see then
+	for line in wallpaper_see:lines() do
+	    local filename = line:match("([^/]+)$")
+	    if filename then
+		table.insert(entries,{
+		    Text = filename:gsub("%.[^.]+$", ""),
+		    Value = line,
+		    Preview = line,
+		    PreviewType = "file",
+		    Actions = { apply = "lua:ApplyWallpaper"}
+		})
+	    end
+	end
+	wallpaper_see:close()
+    end
+    return entries
+end
