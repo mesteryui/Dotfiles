75a0e2793626ae7a028688dd99baac1c73b89d50
Oscar <oscarodriguez56@gmail.com>
2026-05-08 14:58:16 +0200

    Cosas

--- elephant/.config/elephant/menus/wallpapers.lua
+++ elephant/.config/elephant/menus/wallpapers.lua
@@ -5,7 +5,7 @@ Cache = false
 GlobalSearch = false
 
 function ApplyWallpaper(wallpaper)
-	local wallpapers_dir = os.getenv("HOME") .. "/Imágenes/Wallpapers"
+	local wallpapers_dir = os.getenv("HOME") .. "/Bildoj/Wallpapers"
 	os.execute('awww img "' .. wallpaper .. '" --transition-type center')
 	os.execute("cp '" .. wallpaper .. "' " .. wallpapers_dir .. "/.current_wallpaper")
 	os.execute("matugen image '" .. wallpaper .. "' --source-color-index 0")
@@ -13,7 +13,7 @@ end
 
 function GetEntries()
 	local entries = {}
-	local wallpapers_dir = os.getenv("HOME") .. "/Imágenes/Wallpapers"
+	local wallpapers_dir = os.getenv("HOME") .. "/Bildoj/Wallpapers"
 	local find_cmd = "find '"
 		.. wallpapers_dir
 		.. '\' -maxdepth 1 -type f \\( -iname "*.png" -o -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.webp" -o -iname "*.gif" -o -iname "*.svg" \\) 2>/dev/null'
